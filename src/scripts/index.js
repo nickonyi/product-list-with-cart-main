ac98cc33fdfea399c5babc9b879ae8a10c2ecd23
phatommobb <nickoluoch1130@gmail.com>
2024-11-14 23:15:07 +0300

    project setup with webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,32 @@
 import '../styles/style.css'
-import data from '../../data.json'
+import products from '../../data.json'
+
+const productGrid = document.querySelector('#product-grid');
+
+const productDisplay = () => {
+    products.forEach((product) => {
+        productGrid.innerHTML += `
+          <!-- Main image area-->
+          <div class="main-content-area-products">
+          <div class="main-content-img-area" >
+            <img class="img-area" src="${product.image.desktop}" alt="${product.category}">
+            <div class="main-content-img-cart">
+             <img src="../assets/images/icon-add-to-cart.svg" alt="">
+             <span>Add to cart</span>
+            </div>
+         </div>   
+      <!--Description area-->
+           <div class="main-content-description">
+              <h4>${product.name}</h4>
+              <h3>${product.category}</h3>
+              <p class="price">$${product.price}</p>
+           </div>
+          </div> 
+        
+        `
+    })
+}
 
 
-console.log(data);
+productDisplay();
+
