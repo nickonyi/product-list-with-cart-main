38287c5ad8904a1f68770f36857795e499b3b84e
phatommobb <nickoluoch1130@gmail.com>
2024-11-15 17:59:56 +0300

    Add cart js

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,8 +2,6 @@ import '../styles/style.css'
 import products from '../../data.json'
 
 const productGrid = document.querySelector('#product-grid');
-console.log(products[0].image.desktop);
-console.log(window.innerWidth);
 
 const productDisplay = () => { 
    if (window.innerWidth < 440) {
@@ -28,29 +26,29 @@ const productDisplay = () => {
       
       `
   })
-   }
-  
+   } else if (window.innerWidth > 1100) {
     products.forEach((product) => {
-        productGrid.innerHTML += `
-          <!-- Main image area-->
-          <div class="main-content-area-products">
-          <div class="main-content-img-area" >
-            <img class="img-area" src="${product.image.desktop}" alt="${product.category}">
-            <div class="main-content-img-cart">
-             <img src="../assets/images/icon-add-to-cart.svg" alt="">
-             <span>Add to cart</span>
-            </div>
-         </div>   
-      <!--Description area-->
-           <div class="main-content-description">
-              <h4>${product.name}</h4>
-              <h3>${product.category}</h3>
-              <p class="price">$${product.price}</p>
-           </div>
-          </div> 
-        
-        `
-    })
+      productGrid.innerHTML += `
+        <!-- Main image area-->
+        <div class="main-content-area-products">
+        <div class="main-content-img-area" >
+          <img class="img-area" src="${product.image.desktop}" alt="${product.category}">
+          <div class="main-content-img-cart">
+           <img src="../assets/images/icon-add-to-cart.svg" alt="">
+           <span>Add to cart</span>
+          </div>
+       </div>   
+    <!--Description area-->
+         <div class="main-content-description">
+            <h4>${product.name}</h4>
+            <h3>${product.category}</h3>
+            <p class="price">$${product.price}</p>
+         </div>
+        </div> 
+      
+      `
+  })
+   }   
 }
 
 
